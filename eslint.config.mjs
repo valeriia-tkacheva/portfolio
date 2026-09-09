fb0e1fcadea8989f47dc9bd6ce2f516c2642eab9
Maxim Tkachev <maximtkachev99@gmail.com>
2026-09-09 21:28:00 +0300

    prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
-import withNuxt from './.nuxt/eslint.config.mjs'
+import withNuxt from './.nuxt/eslint.config.mjs';
+import prettier from 'eslint-config-prettier/flat';
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt(prettier);
