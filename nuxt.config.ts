68dd821ac86a2d893f7e63879a0d5af2c6981012
Maxim Tkachev <maximtkachev99@gmail.com>
2026-09-11 22:43:10 +0300

    assets/styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: false,
+    },
+  ],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: "@use '~/assets/styles/shared/index.scss' as *;",
+        },
+      },
+    },
+  },
+
+  css: ['~/assets/styles/main.scss'],
 });
